749aa77d9a6e787581d0085fb8cf58161f0ebbbd
xohan30 <gurungaryan73@gmail.com>
2022-07-18 23:17:02 +0530

    add status check for 404

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ from lib import title, imgurl, price, detail
 def Main():
     # Establishing Connection
     link = 'https://www.amazon.es/dp/000106875X'
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    responce = requests.get(link)
+    if responce.status_code == 404:
+        print(link+"not available")
+        pass
+    else:
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
-    source = requests.get(link, headers=headers)
-    soup = BeautifulSoup(source.content, "lxml")
+        source = requests.get(link, headers=headers)
+        soup = BeautifulSoup(source.content, "lxml")
 
-    title.getTitle(soup)
-    imgurl.getImgurl(soup)
-    price.getPrice(soup)
-    detail.getDetail(soup)
+        title.getTitle(soup)
+        imgurl.getImgurl(soup)
+        price.getPrice(soup)
+        detail.getDetail(soup)
 
 
 if __name__ == '__main__':
