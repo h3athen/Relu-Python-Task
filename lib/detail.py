224c13e7e03c4ff14b4431e38d779a419984c5f9
xohan30 <gurungaryan73@gmail.com>
2022-07-18 23:54:23 +0530

    connect csv to link

--- lib/detail.py
+++ lib/detail.py
@@ -3,7 +3,7 @@
 
 def getDetail(soup):
     try:
-        key = [x.get_text(strip=True).replace('\u200f\n','').replace('\u200e','').replace(':\n','').replace('\n', '').strip() for x in soup.select('ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list > li > span > span.a-text-bold')][:13]
+        key   = [x.get_text(strip=True).replace('\u200f\n','').replace('\u200e','').replace(':\n','').replace('\n', '').strip() for x in soup.select('ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list > li > span > span.a-text-bold')][:13]
         value = [x.get_text(strip=True) for x in soup.select('ul.a-unordered-list.a-nostyle.a-vertical.a-spacing-none.detail-bullet-list > li > span > span:nth-child(2)')]
         product_details = {k:v for  k, v, in zip(key, value)}
         print("Product Detail:",product_details)
